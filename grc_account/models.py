37f3e367508841ecfe4c056778b43f731a3d73fa
Borys Mishunin <borys.mishunin@neptuness.com>
2021-05-18 23:14:36 +0300

    dddd

--- grc_account/models.py
+++ grc_account/models.py
@@ -6,38 +6,6 @@ from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 
 
-class AbstractAccount(AbstractUser):
-    """
-    Account model
-    """
-
-    email = models.EmailField(null=True, blank=True)
-    phone = PhoneNumberField(null=True, blank=True)
-    username = models.CharField(max_length=50, unique=True)
-    confirmation_code = models.CharField(max_length=255, null=True, blank=True)
-    is_done = models.BooleanField(default=False)  # is registration done
-    reset_password_token = models.CharField(null=True, unique=True, max_length=30, blank=True)
-    reset_password_token_created_at = models.DateTimeField(null=True)
-    is_active = models.BooleanField(
-        _('active'),
-        default=False,
-        help_text=_(
-            'Designates whether this user should be treated as active. '
-            'Unselect this instead of deleting accounts.'
-        ),
-    )
-
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
-
-    class Meta:
-        db_table = 'users'
-        verbose_name = 'Account'
-        verbose_name_plural = 'Accounts'
-        unique_together = ('email', 'phone')
-        abstract = True
-
-
 class SocialAccount(models.Model):
     """
         Social account model
